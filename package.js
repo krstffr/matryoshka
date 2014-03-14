a581c50fa90e2c4bac651500aa12098dfc48c79d
Kristoffer Klintberg <krstffr@gmail.com>
2014-03-14 16:38:09 +0100

    Working version, finally.

--- package.js
+++ package.js
@@ -1,18 +1,30 @@
 Package.describe({
-  "summary": "A tiny cms for handling stuff."
+  "summary": "A tiny cms for handling recursive js objects or something."
 });
 
 Package.on_use(function (api) {
 
-	api.use('reactive-dict', ['client', 'server']);
-	api.use('deps', ['client', 'server']);
 	api.use('underscore', ['client', 'server']);
-	api.use('ejson', ['client', 'server']);
-
 	api.use('templating', 'client');
 	api.use('handlebars', 'client');
 	api.use('jquery', 'client');
 
-	api.add_files(['client/views/boxcms__rootContainer.html', 'client/views/boxcms__rootContainer.html'], 'client');
-	
+	api.add_files('lib/boxcms__tempStyles.css', 'client');
+
+	api.add_files('lib/boxcms__rootContainer.html', 'client');
+	api.add_files('lib/boxcms__rootContainer.js', 'client');
+
+	api.add_files('lib/boxcms__partLooper.html', 'client');
+	api.add_files('lib/boxcms__partLooper.js', 'client');
+
+	api.add_files('lib/boxcms__globalHelpers.js', 'client');
+	api.add_files('lib/boxcms__router.js', 'client');
+	api.add_files('lib/boxcms__boxcmsHandler.js', 'client');
+
+	if (typeof api.export !== 'undefined') {
+		api.use('iron-router', 'client');
+
+		api.export('Boxcms', ['client']);
+	}
+
 });
