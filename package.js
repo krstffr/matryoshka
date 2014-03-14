2127aed0fe1ebe58c3cd79f49987d8a22cc6f438
Kristoffer Klintberg <krstffr@gmail.com>
2014-03-14 14:46:32 +0100

    Added more stuff to be used on client.

--- package.js
+++ package.js
@@ -4,7 +4,14 @@ Package.describe({
 
 Package.on_use(function (api) {
 
-	api.use(['templating'], 'client');
+	api.use('reactive-dict', ['client', 'server']);
+	api.use('deps', ['client', 'server']);
+	api.use('underscore', ['client', 'server']);
+	api.use('ejson', ['client', 'server']);
+
+	api.use('templating', 'client');
+	api.use('handlebars', 'client');
+	api.use('jquery', 'client');
 
 	api.add_files(['client/views/boxcms__rootContainer.html', 'client/views/boxcms__rootContainer.html'], 'client');
 	
