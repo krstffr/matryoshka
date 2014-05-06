ec7d831583815c54e985d0faff3dea006fd7c90d
Kristoffer Klintberg <krstffr@gmail.com>
2014-05-06 12:39:24 +0200

    Added a focus-mode to nestable parts. Both a button and a URL handler (not quite there yet though).

--- package.js
+++ package.js
@@ -4,56 +4,59 @@ Package.describe({
 
 Package.on_use(function (api) {
 
-	api.use('underscore', ['client', 'server']);
-	api.use('templating', 'client');
-	api.use('handlebars', 'client');
-	api.use('jquery', 'client');
+  api.use('underscore', ['client', 'server']);
+  api.use('templating', 'client');
+  api.use('handlebars', 'client');
+  api.use('jquery', 'client');
 
-	api.add_files('lib/collections/matryoshka__nestables.js', ['server', 'client']);
+  api.add_files('lib/collections/matryoshka__nestables.js', ['server', 'client']);
 
-	api.add_files('lib/matryoshka__methods.js', 'server');
+  api.add_files('lib/matryoshka__methods.js', 'server');
 
-	api.add_files('lib/css/stylesheets/matryoshka__main.css', 'client');
+  api.add_files('lib/css/stylesheets/matryoshka__main.css', 'client');
 
-    api.add_files('lib/views/matryoshka__UImenuButtons.js', 'client');
+  api.add_files('lib/views/matryoshka__UImenuButtons.js', 'client');
 
-    api.add_files('lib/views/matryoshka__login.html', 'client');
-    api.add_files('lib/views/matryoshka__login.js', 'client');
+  api.add_files('lib/views/matryoshka__login.html', 'client');
+  api.add_files('lib/views/matryoshka__login.js', 'client');
 
-    api.add_files('lib/views/matryoshka__fixedButton.html', 'client');
-    api.add_files('lib/views/matryoshka__fixedButton.js', 'client');
+  api.add_files('lib/views/matryoshka__fixedButton.html', 'client');
+  api.add_files('lib/views/matryoshka__fixedButton.js', 'client');
 
-	api.add_files('lib/views/matryoshka__nestablePartButtons.html', 'client');
-    api.add_files('lib/views/matryoshka__nestablePartButtons.js', 'client');
+  api.add_files('lib/views/matryoshka__nestablePartButtons.html', 'client');
+  api.add_files('lib/views/matryoshka__nestablePartButtons.js', 'client');
 
-	api.add_files('lib/views/matryoshka__rootContainer.html', 'client');
-	api.add_files('lib/views/matryoshka__rootContainer.js', 'client');
+  api.add_files('lib/views/matryoshka__nestableSubPartButtons.html', 'client');
+  api.add_files('lib/views/matryoshka__nestableSubPartButtons.js', 'client');
 
-	api.add_files('lib/views/matryoshka__mainMenu.html', 'client');
-	api.add_files('lib/views/matryoshka__mainMenu.js', 'client');
+  api.add_files('lib/views/matryoshka__rootContainer.html', 'client');
+  api.add_files('lib/views/matryoshka__rootContainer.js', 'client');
 
-	api.add_files('lib/views/matryoshka__partLooper.html', 'client');
-	api.add_files('lib/views/matryoshka__partLooper.js', 'client');
+  api.add_files('lib/views/matryoshka__mainMenu.html', 'client');
+  api.add_files('lib/views/matryoshka__mainMenu.js', 'client');
 
-	api.add_files('lib/views/matryoshka__fields.html', 'client');
-	api.add_files('lib/views/matryoshka__fields.js', 'client');
+  api.add_files('lib/views/matryoshka__partLooper.html', 'client');
+  api.add_files('lib/views/matryoshka__partLooper.js', 'client');
 
-	api.add_files('lib/matryoshka__globalHelpers.js', 'client');
+  api.add_files('lib/views/matryoshka__fields.html', 'client');
+  api.add_files('lib/views/matryoshka__fields.js', 'client');
 
-    api.add_files('lib/matryoshka__router.js', 'client');
+  api.add_files('lib/matryoshka__globalHelpers.js', 'client');
 
-	api.add_files('lib/matryoshka__matryoshkaHandler.js', ['client', 'server']);
+  api.add_files('lib/matryoshka__router.js', 'client');
 
-	if (typeof api.export !== 'undefined') {
+  api.add_files('lib/matryoshka__matryoshkaHandler.js', ['client', 'server']);
 
-		api.use(['font-awesome', 'iron-router'], 'client');
+  if (typeof api.export !== 'undefined') {
 
-		// The main object.
-		api.export('Matryoshka', ['server', 'client']);
+    api.use(['font-awesome', 'iron-router'], 'client');
 
-		// The Collection where we store stuff.
-		api.export('MatryoshkaNestables', ['client', 'server']);
+    // The main object.
+    api.export('Matryoshka', ['server', 'client']);
 
-	}
+    // The Collection where we store stuff.
+    api.export('MatryoshkaNestables', ['client', 'server']);
+
+  }
 
 });
