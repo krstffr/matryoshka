fe4c170ec03d8ae650c124cdd266eec5ea1c2c57
Kristoffer Klintberg <krstffr@gmail.com>
2014-09-26 13:13:00 +0200

    Trying to publish packge, some bug with versions...

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
   summary: "A GUI for nesting and linking objects inside other objects.",
   name: "krstffr:matryoshka",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/krstffr/matryoshka.git"
 });
 
 Package.onUse(function (api) {
 
+  // Set versions from.
   api.versionsFrom("METEOR@0.9.0");
 
   // Server/Client stuff
