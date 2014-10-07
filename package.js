fea0fd447c8874b7c8c49b16c020e41afcb8c771
Kristoffer Klintberg <krstffr@gmail.com>
2014-10-07 13:51:49 +0200

    Version bum v.1.0.5

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "A GUI for nesting and linking objects inside other objects.",
   name: "krstffr:matryoshka",
-  version: "1.0.4",
+  version: "1.0.5",
   git: "https://github.com/krstffr/matryoshka.git"
 });
 
