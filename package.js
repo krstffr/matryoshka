bdd376f9cb5747a86dce12dc2d549a72ad6b594c
Kristoffer Klintberg <krstffr@gmail.com>
2014-10-30 17:21:01 +0100

    Added better handling of dates. Also version bump v.1.0.18.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "A GUI for nesting and linking objects inside other objects.",
   name: "krstffr:matryoshka",
-  version: "1.0.17",
+  version: "1.0.18",
   git: "https://github.com/krstffr/matryoshka.git"
 });
 
