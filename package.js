eabedc26cbc0f9e135ec954f95eba9c7388c0a4f
Kristoffer Klintberg <krstffr@gmail.com>
2014-11-17 19:09:04 +0100

    Updated iron:router to 1.0.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "A GUI for nesting and linking objects inside other objects.",
   name: "krstffr:matryoshka",
-  version: "1.1.0",
+  version: "2.0.0",
   git: "https://github.com/krstffr/matryoshka.git"
 });
 
